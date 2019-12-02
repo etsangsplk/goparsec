6ca988554bfe4092c9851539f7c07f62d5bf0b8f
Edward Tsang <etsang@splunk.com>
2019-12-02 14:31:12 -0800

    add comment

--- examples/calculator/calculator_expr.go
+++ examples/calculator/calculator_expr.go
@@ -1,1 +1,14 @@
 package calculator
+
+// This is an example to learn about parser combinator.
+// This is a twist of calculator example.
+// We are going to evaluate simple arithmetic expression written in words
+// For example:
+// *) one plus one
+// *) two muliply five minus five
+// *) negative two
+// *) negative three plus four
+// *) two hundred divide by one hundred
+// etc
+// The answer should be numeric
+// As a bonus write the answer as words
